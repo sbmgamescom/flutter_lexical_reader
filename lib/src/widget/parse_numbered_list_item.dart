6f2bf19f5de786634add2dd20dd043b7b9331415
shakutov <shakutov_b@c.nis.edu.kz>
2023-12-28 15:22:04 +0600

    fix numbered

--- lib/src/widget/parse_numbered_list_item.dart
+++ lib/src/widget/parse_numbered_list_item.dart
@@ -1,16 +1,19 @@
 part of '../parser.dart';
 
-class _ParseNumberedListItem extends StatelessWidget {
-  const _ParseNumberedListItem({
-    required this.child,
-  });
-  final Map<String, dynamic> child;
+// class _ParseNumberedListItem extends StatelessWidget {
+//   const _ParseNumberedListItem({
+//     required this.child,
+//   });
+//   final Map<String, dynamic> child;
 
-  @override
-  Widget build(BuildContext context) {
-    return RichText(
-      text:
-          TextSpan(children: parseJsonChild(child['children'] ?? [], context)),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     final paragraphStyle = _PropsInheritedWidget.of(context)!.paragraphStyle;
+
+//     return RichText(
+//       text: TextSpan(
+//         children: parseJsonChild(child['children'] ?? [], context),
+//       ),
+//     );
+//   }
+// }
